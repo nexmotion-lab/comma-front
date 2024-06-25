149f623d562a845d3264792e5e009e65879c3f3d
chxni <jcg3098@naver.com>
2024-06-25 19:14:14 +0900

    [chore] vuex 감정태그 정보 정의

--- src/main.ts
+++ src/main.ts
@@ -33,10 +33,12 @@ import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import store from "@/store";
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(store);
 
 router.isReady().then(() => {
   app.mount('#app');
